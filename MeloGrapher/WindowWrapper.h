6e410d3d43aee523f7ed4a3201fcf96135b8e4e1
dlgkor <ydhan1024@gmail.com>
2023-07-13 09:28:13 +0900

    Fix: linking bug
    bug happed because some functions was remainning in h file.
    moved them to cpp file and it compiled corretly

--- MeloGrapher/WindowWrapper.h
+++ MeloGrapher/WindowWrapper.h
@@ -27,7 +27,7 @@ public:
 	HINSTANCE hInstance;
 
 	BlockWrapper block_wrapper; //block calculator	
-	
+
 	HWND root_hwnd; //hidden father window. show by tray icon
 	MeloRootWndData root_data;
 	NOTIFYICONDATA nid;
@@ -38,86 +38,9 @@ public:
 public:
 	MeloWindow(HINSTANCE _hInstance);
 	int create_root();
-	int destroy_root();
+	int destroy_root(); //destroy tray icon
 	int create_child(); //create child window
 	int show_child();
 	int wnd_main();
 	~MeloWindow();
 };
-
-MeloWindow::MeloWindow(HINSTANCE _hInstance) {
-	hInstance = _hInstance;
-}
-
-//create root wnd
-int MeloWindow::create_root() {
-	LPCTSTR lpszClass = TEXT("meloGrapher Root");
-
-	WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
-	wcex.lpfnWndProc = WndProc_Root;
-	wcex.hInstance = hInstance;
-	wcex.lpszClassName = lpszClass;
-	RegisterClassEx(&wcex);
-
-	root_hwnd = CreateWindowEx(0, lpszClass, L"MeloGrapher", WS_POPUP, CW_USEDEFAULT, CW_USEDEFAULT,
-		0, 0, NULL, NULL, hInstance, NULL);
-
-	//initialize root data
-	root_data.block_wrapper = &block_wrapper;
-	SetWindowLongPtr(root_hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(&root_data));
-
-	//ShowWindow(root_hwnd, SW_HIDE);
-
-	nid = { sizeof(NOTIFYICONDATA) };
-	nid.hWnd = root_hwnd;
-	nid.uID = 1;
-	nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE;
-	nid.uCallbackMessage = WM_TRAYICON;
-	nid.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MAIN_ICON));
-	wcscpy_s(nid.szTip, L"MeloGrapher Tray Icon");
-
-	Shell_NotifyIcon(NIM_ADD, &nid);
-
-	return 0;
-}
-
-//destroy tray icon of root wnd
-int MeloWindow::destroy_root() {
-	Shell_NotifyIcon(NIM_DELETE, &nid);
-
-	return 0;
-}
-
-int MeloWindow::create_child() {
-
-	return 0;
-}
-
-int MeloWindow::show_child() {
-
-	return 0;
-}
-
-//final wrapper
-int MeloWindow::wnd_main() {
-	MSG Message;
-
-	create_root();
-
-	create_child();
-	show_child();
-
-	//translate and dispatch message
-	while (GetMessage(&Message, NULL, 0, 0)) {
-		TranslateMessage(&Message);
-		DispatchMessage(&Message);
-	}
-
-	destroy_root();
-
-	return (int)Message.wParam;
-}
-
-MeloWindow::~MeloWindow() {
-
-}
