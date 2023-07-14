ab92a3ed614af84d1ef42b29e20ecd18459ed6e1
dlgkor <dlgkor05@gmail.com>
2023-07-14 15:39:40 +0900

    Fix: Link bug
    WindowWrapper.cpp 속성->항목형식 was C/C++ header and it made linker problem.
    Changed C/C++ header format to C/C++ Compiler and now including Windowheader.h in main works well
    I suppose that the problem occured because the file was initialy created with .h format and renamed to .cpp format

--- MeloGrapher/MainWndProc.cpp
+++ MeloGrapher/MainWndProc.cpp
@@ -7,8 +7,7 @@
 */
 LRESULT CALLBACK WndProc_Main(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 	MeloWndData* main_data = reinterpret_cast<MeloWndData*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-	//const char* filename = "C:/Users/ydhan/dlg project/mp3/Hit the Floor.mp3";
-	const char* filename = "C:/Users/ydhan/dlg project/mp3/test.mp3";
+	const char* filename = "D:/mp3/Sneaky Driver.mp3";
 	switch (iMessage) {
 	case WM_CREATE:
 		SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
--- MeloGrapher/main.cpp
+++ MeloGrapher/main.cpp
@@ -1,7 +1,6 @@
 #include"CommonHeader.h"
-#include"WindowWrapper.cpp"
+#include"WindowWrapper.h"
 
-//확인할 수 없는 외부참조 오류는 cpp파일을 include 하니 해결되었다
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
 	MeloWindow melo_window(hInstance);
